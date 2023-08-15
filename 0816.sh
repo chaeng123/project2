083af93546e5868fd2481afdda932a1fc2e18527
chaeng123 <71381080+chaeng123@users.noreply.github.com>
2023-08-16 04:48:02 +0900

    Add files via upload

---
+++ 0816.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name data-analysis
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem-per-gpu=3G
+#SBATCH --time 1-0
+#SBATCH --partition batch_grad
+#SBATCH -o repos/project/logs/slurm-%A-%x.out
+
+python 0816.py
+
+# letting slurm know this code finished without any problem
+exit 0
